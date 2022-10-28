c593c5b65464cfdfb791bd630ca447ea59a01541
고재경 <folw159@gmail.com>
2022-10-28 17:37:21 +0900

    docs: ResultViewController.swift 생성

---
+++ SpendTimeOnTFT/Presentation/Result/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  SpendTimeOnTFT
+//
+//  Created by Jae Kyeong Ko on 2022/10/28.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
