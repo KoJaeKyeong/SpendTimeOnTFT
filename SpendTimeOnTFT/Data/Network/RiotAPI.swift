01a1c4153b4f59162851910969ede578c4746176
고재경 <folw159@gmail.com>
2022-10-31 16:57:50 +0900

    feat: RiotAPI 생성 및 더미 메서드 작성

---
+++ SpendTimeOnTFT/Data/Network/RiotAPI.swift
@@ -0,0 +1,18 @@
+//
+//  RiotAPI.swift
+//  SpendTimeOnTFT
+//
+//  Created by Jae Kyeong Ko on 2022/10/31.
+//
+
+import Foundation
+
+struct RiotAPI {
+    static func summonerInfomation(by name: String) {
+
+    }
+
+    static func matchInfomations(by puuid: String) {
+        
+    }
+}
