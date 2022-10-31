736d373124711cb099f0f55cf741951a67962ccc
고재경 <folw159@gmail.com>
2022-10-31 11:52:02 +0900

    feat: SearchTableViewCell 생성

---
+++ SpendTimeOnTFT/Presentation/Search/SearchTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchTableViewCell.swift
+//  SpendTimeOnTFT
+//
+//  Created by Jae Kyeong Ko on 2022/10/31.
+//
+
+import UIKit
+
+final class SearchTableViewCell: UITableViewCell {
+    
+}
