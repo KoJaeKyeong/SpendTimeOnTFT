b435e0306a64b2e01b871bb4420d10b207a1e8bb
고재경 <folw159@gmail.com>
2022-11-02 13:38:56 +0900

    feat: requestSummonerInfo(by:) 로직 작성

--- SpendTimeOnTFT/Data/Network/RiotAPI.swift
+++ SpendTimeOnTFT/Data/Network/RiotAPI.swift
@@ -8,8 +8,24 @@
 import Foundation
 
 struct RiotAPI {
-    static func summonerInfo(by name: String) {
+    static func requestSummonerInfo(by name: String) async throws -> Summoner? {
+        var components = URLComponents(
+            string: "https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/\(name)?"
+        )
+        let query = URLQueryItem(name: "api_key", value: Bundle.main.riotAPIKey)
+        components?.queryItems?.append(query)
 
+        guard let components = components,
+              let requestURL = components.url else { return nil }
+
+        let (data, response) = try await URLSession.shared.data(from: requestURL)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+              200..<300 ~= httpResponse.statusCode else { return nil }
+
+        let result = try JSONDecoder().decode(Summoner.self, from: data)
+
+        return result
     }
 
     static func matchInfo(by puuid: String) {
