311dccfb8dc4a057017ee21da657491bb9e5d198
고재경 <folw159@gmail.com>
2022-11-03 19:24:39 +0900

    feat: requestMatchInfo(by:) 구현 및 Match, MatchInfo 엔티티 삭제

--- SpendTimeOnTFT/Data/Network/RiotAPI.swift
+++ SpendTimeOnTFT/Data/Network/RiotAPI.swift
@@ -21,7 +21,26 @@ struct RiotAPI {
         return result
     }
 
-    static func matchInfo(by puuid: String) {
+    static func requestMatchInfo(by name: String) async throws -> [String]? {
+        guard let summoner = try await requestSummonerInfo(by: name) else { return nil }
 
+        var startIndex = 0
+        var matchIds = [String]()
+
+        repeat {
+            guard let requestURL = RiotAPIRequestURL.matchURL(by: summoner.puuid, start: startIndex) else { return nil }
+
+            let (data, response) = try await URLSession.shared.data(from: requestURL)
+
+            guard let httpResponse = response as? HTTPURLResponse,
+                  200 == httpResponse.statusCode else { return nil }
+
+            let result = try JSONDecoder().decode([String].self, from: data)
+
+            matchIds.append(contentsOf: result)
+            startIndex += 200
+        } while !matchIds.isEmpty
+
+        return matchIds
     }
 }
