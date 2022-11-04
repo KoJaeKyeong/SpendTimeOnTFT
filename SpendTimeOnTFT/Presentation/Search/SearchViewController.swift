c1f6425985a9dee98625d23c53a653d0a6f639dd
고재경 <folw159@gmail.com>
2022-11-04 14:06:40 +0900

    feat: searchBarDelegate 구현

--- SpendTimeOnTFT/Presentation/Search/SearchViewController.swift
+++ SpendTimeOnTFT/Presentation/Search/SearchViewController.swift
@@ -24,4 +24,26 @@ private extension SearchViewController {
     func configureViews() {
         searchBar.placeholder = "소환사명을 입력해주세요."
     }
+
+    func configureDelegates() {
+        searchBar.delegate = self
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let storyboard = UIStoryboard(name: "Result", bundle: nil)
+        guard let text = searchBar.text,
+              let viewController = storyboard.instantiateViewController(
+                withIdentifier: "Result"
+              ) as? ResultViewController else { return }
+
+        if !text.isEmpty {
+            viewController.summonerName = text
+
+            present(viewController, animated: true)
+        } else {
+            // TODO: Alert 보여주기
+        }
+    }
 }
