604db9e3b32408407a586f0aa7328652c7ee380e
고재경 <folw159@gmail.com>
2022-11-07 20:54:30 +0900

    feat: ResultVC UI 디자인

--- SpendTimeOnTFT/Presentation/Result/ResultViewController.swift
+++ SpendTimeOnTFT/Presentation/Result/ResultViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    
     var summonerName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+}
+
+private extension ResultViewController {
+    func configure() {
+        configureViews()
+    }
+
+    func configureViews() {
+        nameLabel.text = summonerName
     }
 }
