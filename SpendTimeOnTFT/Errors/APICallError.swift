e81a883dbd3ee5de48b3f9701821f835c917008c
고재경 <folw159@gmail.com>
2022-11-08 13:16:45 +0900

    feat: APICallError 작성

---
+++ SpendTimeOnTFT/Errors/APICallError.swift
@@ -0,0 +1,13 @@
+//
+//  APICallError.swift
+//  SpendTimeOnTFT
+//
+//  Created by Jae Kyeong Ko on 2022/11/08.
+//
+
+import Foundation
+
+enum APICallError {
+    case invalidSummonerName
+    case badAccess
+}
